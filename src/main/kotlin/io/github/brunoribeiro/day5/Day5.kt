4629dd63ca83cf3e1c8b019a1778b3ace7682d1f
Bruno Ribeiro <ctw00494@criticaltechworks.com>
2022-12-05 21:40:46 +0000

    initial

--- src/main/kotlin/io/github/brunoribeiro/day5/Day5.kt
+++ src/main/kotlin/io/github/brunoribeiro/day5/Day5.kt
@@ -4,45 +4,44 @@ import java.io.File
 import java.util.InputMismatchException
 
 
-fun main(){
+fun main() {
 
-    val lines =  File("input_5.txt").readLines()
-    val cargo = mutableMapOf<Int,MutableList<String>>()
-   lines.take(8)
-        .map {it.chunked(4)
-            .map { it.replace("]","").replace("[","").replace(" ","") }
-        }
-        .forEach {
-            it.forEachIndexed { index, str ->
-                when(cargo.contains(index + 1)){
-                    true -> cargo[index + 1]?.add(str)
+    val lines = File("input_5.txt").readLines()
+
+    val map = lines.take(8)
+        .map {
+            it.chunked(4)
+                .map { it.replace("]", "").replace("[", "").replace(" ", "") }
+        }.fold(mutableMapOf<Int, MutableList<String>>()) { acc, curr ->
+            curr.foldIndexed(acc) { index, innerAcc, str ->
+                when (innerAcc.contains(index + 1)) {
+                    true -> innerAcc[index + 1]?.add(str)
                     else -> {
-                        cargo[index + 1] = mutableListOf(str)
+                        innerAcc[index + 1] = mutableListOf(str)
                     }
                 }
+                innerAcc
             }
         }
 
-
-    val moves =  lines.subList(10,lines.size)
-
-    moves
-        .map { it.replace("move","").split("from").flatMap { it.split("to") }.map { it.trim() } }
-        .map { Move(it[0].toInt(),it[1].toInt(),it[2].toInt()) }
+    lines.subList(10, lines.size)
+        .map { it.replace("move", "").split("from").flatMap { it.split("to") }.map { it.trim() } }
+        .map { Move(it[0].toInt(), it[1].toInt(), it[2].toInt()) }
         .forEach {
 
-            val fromStack = cargo[it.from].orEmpty().filter { it.isNotBlank() }
-            val targetStack = cargo[it.to].orEmpty().filter { it.isNotBlank() }
+            val fromStack = map[it.from].orEmpty().filter { it.isNotBlank() }
+            val targetStack = map[it.to].orEmpty().filter { it.isNotBlank() }
 
-            cargo[it.to] = ( fromStack.take(it.amount) //.reversed() uncomment for part2 result
+            map[it.to] = (fromStack.take(it.amount) //.reversed() uncomment for part2 result
                     + targetStack) as MutableList<String>
-            cargo[it.from] = fromStack.drop(it.amount).toMutableList()
+            map[it.from] = fromStack.drop(it.amount).toMutableList()
 
+        }.also {
+            println(map.values.joinToString("") { it[0] })
         }
 
-    println(cargo.values.joinToString("") { it[0] })
 
 
 }
 
-data class Move(val amount:Int, val from: Int, val to:Int)
+data class Move(val amount: Int, val from: Int, val to: Int)
